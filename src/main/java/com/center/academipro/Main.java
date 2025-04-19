bbda26273d5172f7ce96aab3ff6a0580fc85892a
Hannh-ff <haiem4259@gmail.com>
2025-04-20 00:21:03 +0700

    Update Main.java

--- src/main/java/com/center/academipro/Main.java
+++ src/main/java/com/center/academipro/Main.java
@@ -2,6 +2,7 @@ package com.center.academipro;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +11,17 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/center/academipro/view/login-view.fxml"));
-        //css
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        // css
 //        scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
 //        scene.getStylesheets().add(getClass().getResource("/css/admin.css").toExternalForm());
 //        scene.getStylesheets().add(getClass().getResource("/css/staff-detail.css").toExternalForm());
 
-        //font
+        // font
 //        Font font = Font.loadFont(getClass().getResourceAsStream("/font/Montserrat-ExtraLight.ttf"), 16);
 
         stage.setTitle("Academi Pro");
@@ -26,7 +30,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
