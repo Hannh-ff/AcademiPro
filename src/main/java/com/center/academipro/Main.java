e16eea7ede5513673efeb762265e201ccca4ea8b
Linhon82 <chaubonuslinh@gmail.com>
2025-04-22 19:17:59 +0700

    đừng mợt về

--- src/main/java/com/center/academipro/Main.java
+++ src/main/java/com/center/academipro/Main.java
@@ -10,7 +10,11 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+<<<<<<< Updated upstream
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("com/center/academipro/view/login-view.fxml"));
+=======
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/center/academipro/view/teacher/attendance-view.fxml"));
+>>>>>>> Stashed changes
         Scene scene = new Scene(fxmlLoader.load());
 
         //css
